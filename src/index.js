15a3e0abb574f1498285514ba74eb06fdabe1104
NadiaKaputkina <nadia.kaputkina@gmail.com>
2019-09-24 18:03:54 +0300

    Task "Brackets" completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strBrackets = str;
+
+    for (let start = 0, i = start; i < strBrackets.length; i = start++) {
+
+        for (let item of bracketsConfig) {
+            if (strBrackets[i] === item[0]) {
+                if (strBrackets[i + 1] === item[1]) {
+                    strBrackets = `${strBrackets.slice(0, i)}${strBrackets.slice(i + 2)}`;
+                    start = 0;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!strBrackets.length) return true;
+    return false;
 }
